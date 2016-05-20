87b1cea8d226f839116557fee4a1f926a8811efb
michael burney <burney318@gmail.com>
2016-05-20 14:49:59 -0500

    thinking about if what im doing is the correct way to go about doing this... should i move forward and see or turn around now?

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,20 +1,21 @@
 var Bob = function() {};
-
-
 Bob.prototype.hey = function(input) {
+    // turn input into an array
+    var arrayInput = input.split();
+    // get last index of array
+    var lastIndex = arrayInput.length - 1;
+    // get last character of array
+    var lastItem = arrayInput[lastIndex];
 
-    return "Whatever."
-
+    if (input == input.toUpperCase()){
+        return "Whoa, chill out!"
+    } else if (lastIndex === String.fromCharCode(33)){
+        return "Sure."
+    } else {
+        return "Whatever."
+    }
 };
 module.exports = Bob;
-
-// // turn input into an array
-// var arrayInput = input.split();
-// // get last index of array
-// var lastIndex = arrayInput.length - 1;
-// // get last character of array
-// var lastItem = arrayInput[lastIndex];
-
 
 
 
